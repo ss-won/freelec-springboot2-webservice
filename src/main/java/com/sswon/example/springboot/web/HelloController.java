f1934a42a32a09af2f2f27fb2ac50459a3946487
Sowon Jung <ss_won_@naver.com>
2020-12-20 23:35:37 +0900

    메인 컨트롤러 작성

---
+++ src/main/java/com/sswon/example/springboot/web/HelloController.java
@@ -0,0 +1,12 @@
+package com.sswon.example.springboot.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String Hello() {
+        return "hello";
+    }
+}
